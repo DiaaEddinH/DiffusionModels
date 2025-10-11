88f1689a166e6058a855502f6cf57774836c3550
LucaKro <luc_kro@uni-bremen.de>
2025-10-11 11:32:19 +0200

    removed hard mac dependency to enable linux compatibility, used mamba on my laptop to automatically resolve these

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,34 +1,44 @@
 import sys
+import unittest
 from pathlib import Path
 
-# # Add src to path if running test directly
-# sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
-
-from src.config_loader import parse_configs
+from config_loader import parse_configs
 
 
-def test_config_loading():
-    # Point to your YAML config
-    config_path = "configs/example_config.yaml"
+class ConfigTestCase(unittest.TestCase):
+    def test_config_loading(self):
+        # Point to your YAML config
+        project_root = Path(__file__).resolve().parents[1]
+        config_path = (project_root / "configs" / "example_config.yaml").as_posix()
 
-    # Simulate CLI args override
-    test_args = ["--config", config_path, "--lr", "0.01", "--batch_size", "128"]
+        # Simulate CLI args override
+        test_args = ["--config", config_path, "--lr", "0.01", "--batch_size", "128"]
 
-    # Patch sys.argv
-    sys.argv = ["test_configs.py"] + test_args
+        # Patch sys.argv
+        sys.argv = ["test_configs.py"] + test_args
 
-    # Parse configs
-    args = parse_configs()
+        # Parse configs
+        args = parse_configs()
 
-    # Print results for manual inspection
-    print("✅ Loaded config + CLI overrides:")
-    for k, v in vars(args).items():
-        print(f"{k}: {v}")
-
-    # Optional: assert some expected overrides
-    assert args.lr == 0.01
-    assert args.batch_size == 128
+        # assert the above values
+        self.assertEqual(args.file, "example")
+        self.assertTrue(args.ddp)
+        self.assertEqual(args.device, "gpu")
+        self.assertEqual(args.lr, 0.01)
+        self.assertEqual(args.max_epochs, 2)
+        self.assertEqual(args.batch_size, 128)
+        self.assertEqual(args.num_workers, 1)
+        self.assertEqual(args.patience, 50)
+        self.assertEqual(args.in_channels, 2)
+        self.assertEqual(args.hidden_channels, [64, 64])
+        self.assertEqual(args.time_channels, 128)
+        self.assertEqual(args.label_dim, 128)
+        self.assertEqual(args.activation.__name__, "LeakyReLU")
+        self.assertEqual(args.sigma_min, 0.02)
+        self.assertEqual(args.sigma_max, 10)
+        self.assertEqual(args.sample_size, 100000)
+        self.assertEqual(args.time_steps, 500)
 
 
 if __name__ == "__main__":
-    test_config_loading()
+    unittest.main()
